a4069293926d72fa6a91f69bd3ee00b55c7738c1
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-07 17:38:01 +0400

    updated home page

--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{2023}</time> © Avinash Lingaloo.
       <style jsx>{`
         a {
           float: right;
